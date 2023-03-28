178b72982edd099f9246a55dee4e5504f906711a
said.montufar <said.montufar@outlook.com>
2023-03-28 17:43:52 -0500

    Fix stone counter

---
+++ lib/game_of_stones/game.rb
@@ -0,0 +1,39 @@
+module GameOfStones
+  class Game
+    attr_accessor :players, :pile
+
+
+    def initialize
+      welcoming_message
+      @players = initialize_players
+      @pile = GameOfStones::Models::Pile.new
+
+    end
+
+    def start!
+      players.cycle do |player|
+        player.make_turn!(pile)
+
+        if pile.empty?
+          player.lost!
+          break
+        end
+      end
+    end
+
+    private
+
+    def initialize_players
+      2.times.map do |i|
+        puts "Player #{i + 1}, please enter your name:"
+        GameOfStones::Models::Player.new(gets.strip)
+      end
+    end
+
+    def welcoming_message
+      puts '*' * 29
+      puts 'WELCOME TO THE GAME'
+      puts '*' * 29
+    end
+  end
+end
---
+++ lib/game_of_stones/models/player.rb
@@ -0,0 +1,37 @@
+module GameOfStones
+  module Models
+    class Player
+      attr_reader :name
+
+      def initialize(name)
+        @name = name
+      end
+
+      def make_turn!(pile)
+        puts "Current number of stones is #{pile.stones}"
+        puts "Player #{self.name}, it is your turn now!"
+
+        take_stones_from(pile)
+      end
+
+      def lost!
+        puts "Player #{name} lost"
+      end
+
+      private
+
+      def take_stones_from(pile)
+        number = loop do
+          number = gets.to_i
+          if pile.has_enough_stones?(number)
+            break number
+          else
+            puts "Please enter the correct number"
+          end
+        end
+        pile.take!(number)
+      end
+
+    end
+  end
+end
